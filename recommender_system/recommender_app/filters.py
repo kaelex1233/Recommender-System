a94280eb837554dd0c62fc8843187143a13bb92f
Kent Levi <kentlevi.cadungog@ctu.edu.ph>
2021-09-27 14:11:26 +0800

    Update filters.py

--- recommender_system/recommender_app/filters.py
+++ recommender_system/recommender_app/filters.py
@@ -1,13 +1,13 @@
 import django_filters
 from django_filters import DateFilter, CharFilter
 
-class OrderFilter(django_filters.FilterSet):
-	start_date = DateFilter(field_name="date_created", lookup_expr='gte')
-	end_date = DateFilter(field_name="date_created", lookup_expr='lte')
-	note = CharFilter(field_name='note', lookup_expr='icontains')
+class StudentInfoFilter(django_filters.FilterSet):
+	name = CharFilter(field_name='name', lookup_expr='icontains')
+	job_description = CharFilter(field_name='job_description', lookup_expr='icontains')
+	skills = CharFilter(field_name='skills', lookup_expr='icontains')
 
 
 	class Meta:
-		model = Students
+		model = Student
 		fields = '__all__'
-		exclude = ['customer', 'date_created']
+		exclude = ['phone', 'email', 'profile_picture', 'date_created']
