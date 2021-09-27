1f82b80033384d93906816e0d4a8a58544572d98
Kent Levi <kentlevi.cadungog@ctu.edu.ph>
2021-09-27 14:28:20 +0800

    Update views.py

--- recommender_system/recommender_app/views.py
+++ recommender_system/recommender_app/views.py
@@ -5,9 +5,12 @@ from .models import *
 # Create your views here.
 
 def display_students(request):
-    student = Student.objects.all()
+    students = Student.objects.all()
 
-    context = {'student': student}
+    myFilter = OrderFilter(request.GET, queryset=students)
+    students = myFilter.qs
+
+    context = {'students': students, 'myFilter': myFilter}
     return render(request, 'recommender_app/display_students.html', context)
 
 def display_student_story(request, pk):
